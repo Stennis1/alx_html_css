9c7900665bc71181b6157de53fca4e679aec4376
Stennis1 <stennismith@gmail.com>
2025-08-09 02:12:42 +0000

    feat: add initial HTML structure and styles for headphones online shop

---
+++ 8-script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+    const navItems = navLinks.querySelector('a');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+
+        // Change symbol 
+        if (navLinks.classList.contains('show')) {
+            menuToggle.innerHTML = '&times';
+            menuToggle.style.fontSize = '3rem';
+        } else {
+            menuToggle.innerHTML = '&#9776';
+        }
+    });
+
+    navItems.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('show');
+            menuToggle.innerHTML = '&#9776';
+        });
+
+    });
+});
